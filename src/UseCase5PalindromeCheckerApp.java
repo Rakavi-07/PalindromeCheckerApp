3d6e7334b8878e9b1c2f0a7a2a168a11ca44cea4
Rakavi R <rrakavi25@gmail.com>
2026-02-20 10:34:43 +0530

    UC5: Stack-based palindrome checker

---
+++ src/UseCase5PalindromeCheckerApp.java
@@ -0,0 +1,32 @@
+
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "noon";
+
+        // Create a Stack to store characters
+        Stack<Character> stack = new Stack<>();
+
+        // Push each character into the stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        // Pop characters and compare
+        for (char c : input.toCharArray()) {
+
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
