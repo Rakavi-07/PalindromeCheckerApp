88ba465e8a058d3d405080f032d3a05fbcf07745
Rakavi R <rrakavi25@gmail.com>
2026-03-08 00:57:00 +0530

    UC13: Performance Comparison

---
+++ src/UseCase13PalindromeCheckerApp.java
@@ -0,0 +1,40 @@
+public class UseCase13PalindromeCheckerApp {
+
+    // Palindrome check method
+    public static boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        // Start time
+        long startTime = System.nanoTime();
+
+        boolean result = checkPalindrome(input);
+
+        // End time
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + executionTime + " ns");
+    }
+}
