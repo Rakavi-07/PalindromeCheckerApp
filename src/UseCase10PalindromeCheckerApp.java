ea8dabf84474f44f3f11fb20b5319ec6b478f1d7
Rakavi R <rrakavi25@gmail.com>
2026-03-08 00:36:42 +0530

    UC10 - Case-Insensitive & Space-Ignored Palindrome

---
+++ src/UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,24 @@
+public class UseCase10PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "A man a plan a canal Panama";
+
+        // Normalize the string
+        String normalized = input.toLowerCase().replaceAll("\\s+", "");
+
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
